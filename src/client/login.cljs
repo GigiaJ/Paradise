a90daa5ec88c6b19d3b6492a5ffc257a11aaad96
GigiaJ <gigiaboone@yahoo.com>
2026-02-18 01:59:32 -0600

    Continue on trying RoomListView

--- src/client/login.cljs
+++ src/client/login.cljs
@@ -1,42 +1,41 @@
 (ns client.login
   (:require
+   [client.view-models :refer [create-room-list-vm]]
    [reagent.core :as r]
    [promesa.core :as p]
    [client.state :refer [sdk-world]]
    [client.sdk-ctrl :as sdk-ctrl]
-   ["generated-compat" :as sdk])
+   ["generated-compat" :as sdk]
+   ["@element-hq/web-shared-components" :refer [RoomListView BaseViewModel]])
   (:require-macros [macros :refer [ocall oget]])
   )
 
 (defonce sdk-ready? (r/atom false))
+  (defn init-sdk! []
+    (-> (p/let [_ (sdk/uniffiInitAsync)]
+          (js/console.log "WASM loaded")
+          (reset! sdk-ready? true)
+          (swap! sdk-world assoc :loading? false))
+        (p/catch (fn [e]
+                   (js/console.error "WASM Load Failed:" e)
+                   (swap! sdk-world assoc :loading? false)))))
 
-(defn init-sdk! []
-  (-> (p/let [_ (sdk/uniffiInitAsync)]
-        (js/console.log "WASM loaded")
-        (reset! sdk-ready? true)
-        (swap! sdk-world assoc :loading? false))
-      (p/catch (fn [e]
-                 (js/console.error "WASM Load Failed:" e)
-                 (swap! sdk-world assoc :loading? false)))))
-
-(defn start-sync! [client on-success]
-  (-> (p/let [rls-service (ocall client :roomListService)
-              rls (ocall rls-service :allRooms)
-              sync-service (ocall rls :syncService)
-              sync-handle (-> sync-service (.withOfflineMode) (.finish))
-              _ (.start ^js sync-handle)]
-        (js/console.log "RoomListService & Sync bonded")
-        ;; TODO wrap proper for type validation
-        (swap! sdk-world assoc 
-               :client (client.state/->MatrixClient client) 
-               :loading? false)
-        (client.sdk-ctrl/setup-room-list! rls)
-        (on-success client))
-      (p/catch 
-          (fn [e]
-            (js/console.error "Sync Chain Failed:" e)
-            (swap! sdk-world assoc :loading? false)))))
-
+(defn start-sync! [client]
+  (p/let [sync-service (-> (.syncService client) (.withOfflineMode) (.finish))
+          rls-instance (.roomListService sync-service)
+          room-list    (.allRooms rls-instance)]
+    (let [rl-vm (create-room-list-vm #js {:client client :roomListService rls-instance})]
+      (do
+        (aset rl-vm "onUpdate"
+              (fn [updates] (js/console.log "UPDATE TRIGGERED!" (alength updates))))
+        (let [entries-result (.entriesWithDynamicAdapters room-list 200 rl-vm)]
+            (aset rl-vm "entries_handle" (.entriesStream entries-result))
+            (aset rl-vm "controller" (.controller entries-result)))
+          (swap! sdk-world assoc-in [:vms :room-list] rl-vm)
+          (.start sync-service)
+          (.setRange  (.-controller rl-vm) 0 50)
+          (.addOnePage (.-controller rl-vm))
+          (js/console.log "Background stream handle secured. Syncing...")))))
 
 (defn login! [hs user pass on-success on-rooms-update]
   (let [sdk-root (if (.-Client sdk) sdk (.-default sdk))
