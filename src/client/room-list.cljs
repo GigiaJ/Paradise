89e615c2d6c537ce6573502e273cea6a85a4fea5
GigiaJ <gigiaboone@yahoo.com>
2026-02-18 02:01:49 -0600

    Swap to auto-gen w/ org-mode tangle

--- src/client/room-list.cljs
+++ src/client/room-list.cljs
@@ -7,15 +7,15 @@
   (let [all-rooms (:all-rooms @sdk-world)
         FilterKind (.-RoomListEntriesDynamicFilterKind sdk)]
     (p/let [
-            res (.entriesWithDynamicAdapters ^js all-rooms 200 
+            res (.entriesWithDynamicAdapters ^js all-rooms 200
                                              #js {:onUpdate (fn [updates] (handle-room-diff! updates))})
             ctrl (.controller ^js res)
-            ldr (.loadingState ^js all-rooms 
+            ldr (.loadingState ^js all-rooms
                                #js {:onUpdate (fn [s] (handle-loading! s ctrl))})]
       (swap! sdk-world assoc :ctrl ctrl)
       (swap! sdk-world assoc-in [:handles :entries] res)
       (swap! sdk-world assoc-in [:handles :loading] ldr)
 
-      (.setFilter ^js ctrl 
+      (.setFilter ^js ctrl
                   (.All FilterKind #js {:filters #js [(new (.-NonLeft FilterKind))]}))
       (.addOnePage ^js ctrl))))
