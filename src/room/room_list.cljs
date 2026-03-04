2debc8cc519911d598b199a6fc6882d30c2e37c4
GigiaJ <gigiaboone@yahoo.com>
2026-03-04 06:50:53 -0600

    Redid Roomlist a bunch, now handles subspace rendering and fully loads

--- src/room/room_list.cljs
+++ src/room/room_list.cljs
@@ -1,5 +1,7 @@
 (ns room.room-list
-  (:require [promesa.core :as p]
+  (:require
+   [clojure.string :as str]
+   [promesa.core :as p]
             [re-frame.core :as re-frame]
             [taoensso.timbre :as log]
             [re-frame.db :as db]
@@ -9,37 +11,114 @@
             [reagent.dom.client :as rdom]
             [room.room-summary :refer [build-room-summary]]
             [client.diff-handler :refer [apply-matrix-diffs]]
-            ["generated-compat" :as sdk :refer [RoomListEntriesDynamicFilterKind RoomListLoadingState]])
+            [utils.global-ui :refer [avatar]]
+            ["generated-compat" :as sdk :refer [RoomListEntriesDynamicFilterKind RoomListFilterCategory RoomListLoadingState]])
   (:require-macros [utils.macros :refer [ocall oget]]))
 
 
-(defn- parse-room [room-interface]
-  (p/let [room-info     (.roomInfo room-interface)
+(defn parse-room [space-service room-interface]
+  (p/let [room-info     (if (fn? (.-roomInfo room-interface)) (.roomInfo room-interface) nil)
           latest-event  (.latestEvent room-interface)
-          room-id       (.-id room-info)]
+          room-id       (.-id room-info)
+           parents (if space-service
+                    (.joinedParentsOfChild space-service room-id)
+                    (p/resolved []))
+          first-parent (get parents 0)
+          first-parent-id (if first-parent (.-roomId first-parent) nil)
+          ]
     (let [summary (build-room-summary room-interface room-info latest-event)]
-      (aset summary "raw_room" room-interface)
+      (aset summary "raw-room" room-interface)
+      (aset summary "parents" parents)
+      (log/debug summary)
+      (aset summary "first-parent-id" first-parent-id)
       summary)))
 
+(defonce !room-mutex (atom (p/resolved nil)))
+
 (defn apply-global-diffs-async! [updates]
-  (let [current-rooms (get @re-frame.db/app-db :global-rooms [])]
-    (-> (apply-matrix-diffs current-rooms updates #(parse-room %))
-        (.then (fn [next-rooms]
-                 (re-frame/dispatch [:sdk/set-global-rooms next-rooms])))
-        (.catch #(js/console.error "Global Diff Panic:" %)))))
+  (swap! !room-mutex
+         (fn [prev-promise]
+           (p/then prev-promise
+                   (fn []
+                     (let [db @re-frame.db/app-db
+                           current-rooms (get db :rooms [])
+                           space-service (:space-service db)]
+                       (-> (apply-matrix-diffs current-rooms updates #(parse-room space-service %))
+                           (p/then (fn [next-rooms]
+                                     (re-frame/dispatch-sync [:sdk/set-global-rooms-sync next-rooms])
+                                     (re-frame/dispatch [:sdk/set-global-rooms next-rooms])))
+                           (p/catch (fn [err]
+           (log/error "Global Diff Panic:" err)
+           nil)))))))))
+
+(defn get-rust-filter [filter-id]
+  (case filter-id
+    "unread"
+    (RoomListEntriesDynamicFilterKind.All.
+      #js {:filters #js [(RoomListEntriesDynamicFilterKind.Unread.)
+                         (RoomListEntriesDynamicFilterKind.DeduplicateVersions.)]})
+    "people"
+    (RoomListEntriesDynamicFilterKind.All.
+      #js {:filters #js [(RoomListEntriesDynamicFilterKind.Category. #js {:expect RoomListFilterCategory.People})
+                         (RoomListEntriesDynamicFilterKind.Joined.)
+                         (RoomListEntriesDynamicFilterKind.DeduplicateVersions.)]})
+    "favourite"
+    (RoomListEntriesDynamicFilterKind.All.
+      #js {:filters #js [(RoomListEntriesDynamicFilterKind.Favourite.)
+                         (RoomListEntriesDynamicFilterKind.DeduplicateVersions.)]})
+
+    (RoomListEntriesDynamicFilterKind.All.
+      #js {:filters #js [(RoomListEntriesDynamicFilterKind.NonLeft.)
+                         (RoomListEntriesDynamicFilterKind.DeduplicateVersions.)]})))
 
 (defn start-room-list-sync! [room-list]
+  (log/debug "Starting Room List Sync!")
   (let [entries-result (.entriesWithDynamicAdapters room-list 200
-                         #js {:onUpdate #(re-frame/dispatch [:sdk/global-rooms-diff %])})
-        controller (.controller entries-result)
-        non-left (RoomListEntriesDynamicFilterKind.NonLeft.)
-        dedup (RoomListEntriesDynamicFilterKind.DeduplicateVersions.)
-        filter-all (RoomListEntriesDynamicFilterKind.All. #js {:filters #js [non-left dedup]})]
-    (.setFilter controller filter-all)
-    (.addOnePage controller)
+                         #js {:onUpdate #(re-frame/dispatch [:sdk/global-rooms-diff %])})]
+    (re-frame/dispatch [:sdk/save-room-list-handle entries-result])
+    (let [controller (.controller entries-result)
+          default-filter (get-rust-filter "people")]
+      (.setFilter controller default-filter)
+      (.addOnePage controller))
     (let [loading-result (.loadingState room-list
                            #js {:onUpdate #(re-frame/dispatch [:sdk/room-list-loading %])})]
       (re-frame/dispatch [:sdk/room-list-loading (.-state loading-result)]))))
+
+(defn distinct-by [f coll]
+  (let [!seen (atom #{})]
+    (filterv (fn [x]
+               (let [k (f x)]
+                 (if (contains? @!seen k)
+                   false
+                   (do (swap! !seen conj k)
+                       true))))
+             coll)))
+
+(defn flatten-tree [rooms-map children-map closed-drawers parent-id depth]
+  (let [sdk-children   (get children-map parent-id)
+        manual-children (->> (vals rooms-map)
+                             (filter #(= (:first-parent-id %) parent-id))
+                              (map (fn [r] {:id (or (:id r) (:roomId r)) :is-space? (:isSpace r)}))
+                              (vec))
+        child-refs (->> (concat sdk-children manual-children)
+                        (distinct-by :id)
+                        (vec))]
+    (reduce
+      (fn [acc {:keys [id is-space?]}]
+        (let [rich-data (get rooms-map id)
+              item      (merge rich-data
+                               {:id id
+                                :is-space? is-space?
+                                :depth depth
+                                :name (or (:name rich-data) "Loading...")})
+              acc-with-item (conj acc item)]
+          (if (and is-space? (not (contains? closed-drawers id)))
+            (into acc-with-item (flatten-tree rooms-map children-map closed-drawers id (inc depth)))
+            acc-with-item)))
+      []
+      child-refs)))
+
+
 
 (re-frame/reg-sub
  :rooms/active-id
@@ -58,16 +137,6 @@
    (vec (or (:space-rooms db) []))))
 
 (re-frame/reg-sub
- :rooms/current-view
- :<- [:spaces/active-id]
- :<- [:space-rooms-map]
- :<- [:rooms/all]
- (fn [[active-id space-rooms-map all-rooms] _]
-   (if active-id
-     (get space-rooms-map active-id [])
-     all-rooms)))
-
-(re-frame/reg-sub
  :rooms/active-metadata
  :<- [:rooms/active-id]
  :<- [:rooms/all]
@@ -76,12 +145,49 @@
    (when active-id
      (let [all-known (into all-rooms space-rooms)]
        (some (fn [r]
-               (let [rid (or (try (.-roomId r) (catch :default _ nil))
-                             (try (.-id r) (catch :default _ nil))
-                             (:roomId r)
-                             (:id r))]
+               (let [rid (or (:id r)
+                             (try (.-roomId r) (catch :default _ nil))
+                             (try (.-id r) (catch :default _ nil)))]
                  (when (= rid active-id) r)))
              all-known)))))
+
+
+(re-frame/reg-sub
+ :rooms/map
+ :<- [:rooms/all]
+ (fn [rooms-list _]
+   (into {}
+         (for [room rooms-list]
+           (let [r  (js->clj room :keywordize-keys true)
+                 id (or (:id r) (:roomId r))]
+             [id r])))))
+
+
+(re-frame/reg-sub
+ :rooms/current-view
+ :<- [:spaces/active-id]
+ :<- [:rooms/map]
+ :<- [:space-children/map]
+ :<- [:rooms/closed-drawers]
+ (fn [[active-space-id rooms-map children-map closed-drawers] _]
+   (if-not active-space-id
+     (vec (vals rooms-map))
+     (flatten-tree rooms-map children-map closed-drawers active-space-id 0))))
+
+
+(re-frame/reg-event-db
+ :rooms/toggle-drawer
+ (fn [db [_ drawer-id]]
+   (update-in db [:ui :closed-drawers]
+              (fn [closed]
+                (if (contains? closed drawer-id)
+                  (disj closed drawer-id)
+                  (conj (or closed #{}) drawer-id))))))
+
+(re-frame/reg-sub
+ :rooms/closed-drawers
+ (fn [db _]
+   (get-in db [:ui :closed-drawers] #{})))
 
 (re-frame/reg-event-fx
  :sdk/global-rooms-diff
@@ -90,22 +196,58 @@
    {}))
 
 (re-frame/reg-event-db
- :sdk/set-global-rooms
+ :sdk/set-global-rooms-sync
  (fn [db [_ rooms]]
    (assoc db :rooms rooms)))
+
+(re-frame/reg-event-fx
+ :sdk/set-global-rooms
+ (fn [_ [_ rooms]]
+   (let [top-room-ids (take 2 (map #(.-id %) rooms))]
+     {:dispatch [:sdk/preload-timelines top-room-ids]})))
 
 (re-frame/reg-event-db
  :sdk/room-list-loading
  (fn [db [_ state-obj]]
    (assoc db :rooms-loading? (= "NotLoaded" (.-tag state-obj)))))
 
+(re-frame/reg-sub
+ :rooms/active-room-members
+ :<- [:rooms/active-id]
+ (fn [active-id db]
+   (get-in db [:rooms/members active-id] [])))
+
+(re-frame/reg-event-fx
+ :rooms/fetch-members
+ (fn [{:keys [db]} [_ room-id]]
+   (let [client (:client db)]
+     (when (and client room-id)
+       (if-let [room (.getRoom client room-id)]
+         (p/let [members (.membersNoSync room)
+                 parsed  (map (fn [m]
+                                {:user-id      (.userId m)
+                                 :display-name (or (.displayName m) (.userId m))
+                                 :avatar-url   (.avatarUrl m)})
+                              members)]
+           (re-frame/dispatch [:rooms/save-members room-id parsed]))
+         (log/warn "Could not find room in client for members fetch:" room-id))))
+   {}))
+
+(re-frame/reg-event-db
+ :rooms/save-members
+ (fn [db [_ room-id members]]
+   (assoc-in db [:rooms/members room-id] members)))
+
+
 (re-frame/reg-event-fx
  :room/select
  (fn [{:keys [db]} [_ room-id]]
    (let [current-room (:active-room-id db)]
      (if (= current-room room-id)
-       {}
-       {:db (assoc db :active-room-id room-id)
+       {:db (assoc-in db [:ui :sidebar-open?] false)}
+       {:db (-> db
+                (assoc :active-room-id room-id)
+                (assoc-in [:ui :sidebar-open?] false))
         :dispatch-n (remove nil?
                             [(when current-room [:sdk/cleanup-timeline current-room])
                              [:sdk/boot-timeline room-id]
@@ -126,39 +268,107 @@
    (let [global-rooms (vals (:rooms db))]
      {:db (assoc db :space-rooms (vec global-rooms))})))
 
+(re-frame/reg-event-db
+ :sdk/save-room-list-handle
+ (fn [db [_ result]]
+   (assoc db
+     :room-list-result result
+     :room-list-controller (.controller result))))
 
-(defn virtualized-room-list [room-array space-id active-room]
-  [:> Virtuoso
-   {:style {:height "100%" :width "100%"}
-    :data room-array
-    :endReached #(when space-id (re-frame/dispatch [:sdk/paginate-space space-id]))
-    :computeItemKey (fn [index room]
-                      (if room
-                        (or (.-roomId room) (.-id room) index)
-                        index))
-    :itemContent (fn [index room]
-                   (r/as-element
-                    (if room
-                      (let [id   (or (.-roomId room) (.-id room))
-                            name (or (.-displayName room) (.-name room) "Unknown Room")]
-                        (when id
-                          [:div.room-item
-                           {:class (when (= id active-room) "active")
-                            :key id
-                            :on-click #(re-frame/dispatch [:room/select id])}
-                           [:span.room-hash "# "]
-                           [:span.room-name name]]))
-                      [:div "Loading..."])))}])
+(re-frame/reg-event-db
+ :sdk/save-room-list-controller
+ (fn [db [_ controller]]
+   (assoc db :room-list-controller controller)))
+
+(re-frame/reg-event-db
+ :room-list/set-filter
+ (fn [db [_ filter-id]]
+   (assoc db :active-filter-id filter-id)))
+
+(re-frame/reg-sub
+ :room-list/active-filter
+ (fn [db _]
+   (get db :active-filter-id "people")))
+
+(re-frame/reg-event-fx
+ :room-list/apply-filter
+ (fn [{:keys [db]} [_ filter-id]]
+   (let [current-filter (:active-filter-id db)
+         controller     (:room-list-controller db)]
+     (cond
+       (= current-filter filter-id)
+       (do (log/debug "Filter already set to:" filter-id)
+           {})
+
+       (not controller)
+       (do (log/warn "Filter swap failed: No room-list-controller found.")
+           {})
+
+       :else
+       (do
+         (log/debug "Swapping filter from" current-filter "to" filter-id)
+         (.setFilter controller (get-rust-filter filter-id))
+         (.addOnePage controller)
+         {:db (assoc db :active-filter-id filter-id)})))))
+
+(defn filter-toggle-bar []
+  (let [active-filter @(re-frame/subscribe [:room-list/active-filter])]
+    [:div.filter-bar
+     (for [[id label] [["all" "All"] ["unread" "Unread"] ["people" "People"]]]
+       ^{:key id}
+       [:button.filter-btn
+        {:class    (when (= active-filter id) "active")
+         :on-click #(re-frame/dispatch [:room-list/apply-filter id])}
+        label])]))
+
+(defn virtualized-room-list [room-array space-id active-room closed-drawers]
+  (let [active-filter @(re-frame/subscribe [:room-list/active-filter])]
+    [:> Virtuoso
+     {:style {:height "100%" :width "100%"}
+      :data room-array
+      :endReached #(re-frame/dispatch [:room-list/paginate-global])
+      :itemContent
+      (fn [index room]
+        (r/as-element
+         (let [{:keys [id name is-space? depth notification-count avatar-url]} room
+               depth   (or depth 0)
+               indent  (* depth 12)
+               is-people? (or (= active-filter "people")
+                              (and (not is-space?) (some? avatar-url)))
+               is-closed? (contains? closed-drawers id)]
+           (if is-space?
+             [:div.room-drawer-header
+              {:style {:padding-left (str indent "px")}
+               :class (when is-closed? "collapsed")
+               :on-click #(re-frame/dispatch [:rooms/toggle-drawer id])}
+              [:span.drawer-arrow "▼"]
+              [:span.drawer-name (str/upper-case name)]]
+             [:div.room-item
+              {:style {:padding-left (str indent "px")}
+               :class (when (= id active-room) "active")
+               :on-click #(re-frame/dispatch [:room/select id])}
+              (if is-people?
+                 [avatar {:id id :name name :url avatar-url :size 24 :status :online}]
+                 [:span.room-hash "# "])
+              [:span.room-name name]
+              (when (> (or notification-count 0) 0)
+                [:div.badge notification-count])]))))}]))
 
 (defn room-list []
-  (let [rooms @(re-frame/subscribe [:rooms/current-view])
-        active-room @(re-frame/subscribe [:rooms/active-id])
-        active-space @(re-frame/subscribe [:spaces/active-id])
-        room-array (to-array rooms)] 
+  (let [rooms          @(re-frame/subscribe [:rooms/current-view])
+        active-room    @(re-frame/subscribe [:rooms/active-id])
+        active-space-id   @(re-frame/subscribe [:spaces/active-id])
+        active-space    @(re-frame/subscribe [:spaces/active-metadata])
+        closed-drawers @(re-frame/subscribe [:rooms/closed-drawers])
+        room-array     (to-array rooms)]
     [:div.sidebar-rooms
      {:style {:display "flex" :flex-direction "column" :height "100%"}}
-     [:h3.rooms-header "Rooms"]
+     [:h3.rooms-header {:style {:padding "8px"}}
+      (if active-space
+        [:<> [:h3 {:style {:margin 0 :font-size "1.1rem"}} (:name active-space)]])]
+     (when-not active-space
+       [filter-toggle-bar])
      (if (or (nil? room-array) (== (alength room-array) 0))
        [:div.empty-state "No rooms here..."]
        [:div.room-collection {:style {:flex 1 :min-height 0}}
-        [virtualized-room-list room-array active-space active-room]])]))
+        [virtualized-room-list room-array active-space active-room closed-drawers]])]))
