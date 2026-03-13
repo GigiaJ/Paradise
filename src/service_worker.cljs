9d4e23b163bfe0e58e7a1233e80017d443547a7b
GigiaJ <gigiaboone@yahoo.com>
2026-03-13 05:28:46 -0500

    Add auto-generated files and comment a line out

--- src/service_worker.cljs
+++ src/service_worker.cljs
@@ -1,7 +1,8 @@
 (ns service-worker
   (:require [clojure.string :as str]
             [promesa.core :as p]
-            ["workbox-precaching" :refer [precacheAndRoute]]))
+            ["workbox-precaching" :refer [precacheAndRoute cleanupOutdatedCaches]]
+            ))
 
 (defonce active-sessions (atom {}))
 (defonce session-resolvers (atom []))
@@ -29,6 +30,28 @@
     (p/resolved true)
     (js/Promise. (fn [resolve _]
                    (swap! session-resolvers conj resolve)))))
+
+(defn- get-token-from-vault [user-id]
+  (p/create
+   (fn [resolve _]
+     (let [req (.open js/indexedDB "sw-vault" 1)]
+       (set! (.-onsuccess req)
+             (fn [e]
+                 (let [db (.. e -target -result)
+                       tx (try (.transaction db #js ["tokens"] "readonly") (catch :default _ nil))
+                       store (when tx (.objectStore tx "tokens"))
+                       get-req (if (and store user-id)
+                                 (.get store user-id)
+                                 (when store (.getAll store)))]
+                   (if-not get-req
+                     (resolve nil)
+                     (set! (.-onsuccess get-req)
+                           (fn [res]
+                             (let [result (.. res -target -result)]
+                               (if (array? result)
+                                 (resolve (some-> (first result) .-token))
+                                 (resolve (some-> result .-token))))))))))
+       (set! (.-onerror req) #(resolve nil))))))
 
 (defn prune-cache! [name max-items]
   (p/let [cache (js/caches.open name)
@@ -88,3 +111,114 @@
     (.waitUntil event
       (p/all [(.clients.claim js/self)
               (prune-cache! cache-name MAX_CACHE_ITEMS)]))))
+
+(def default-icon "/public/res/apple/apple-touch-icon-180x180.png")
+(def default-badge "/public/res/apple/apple-touch-icon-72x72.png")
+
+(defn- get-session-from-vault [user-id]
+  (p/create
+   (fn [resolve reject]
+     (let [req (.open js/indexedDB "sw-vault" 1)]
+       (set! (.-onsuccess req)
+             (fn [e]
+               (let [db    (.. e -target -result)
+                     tx    (.transaction db #js ["tokens"] "readonly")
+                     store (.objectStore tx "tokens")]
+                 (if user-id
+                   (let [get-req (.get store user-id)]
+                     (set! (.-onsuccess get-req) #(resolve (.. % -target -result)))
+                     (set! (.-onerror get-req) #(resolve nil)))
+                   (let [all-req (.getAll store)]
+                     (set! (.-onsuccess all-req)
+                           (fn [ae]
+                             (let [results (vec (.. ae -target -result))]
+                               (if (empty? results)
+                                 (resolve nil)
+                                 (resolve (->> results
+                                               (sort-by #(.-updated_at %) >)
+                                               first)))))))))))))))
+
+
+
+(defn- fetch-avatar-url [hs-url token mxc-uri]
+  (if (and hs-url token mxc-uri)
+    (let [media-id (clojure.string/replace mxc-uri "mxc://" "")
+          thumb-url (str hs-url "/_matrix/media/v3/thumbnail/" media-id
+                         "?width=96&height=96&method=crop")]
+      (-> (js/fetch thumb-url #js {:headers #js {:Authorization (str "Bearer " token)}})
+          (p/then (fn [resp]
+                    (if (.-ok resp)
+                      (p/let [blob (.blob resp)]
+                        (js/URL.createObjectURL blob))
+                      nil)))
+          (p/catch (fn [_] nil))))
+    (p/resolved nil)))
+
+(defn- fetch-event-content [hs-url token room-id event-id]
+  (let [url (str hs-url "/_matrix/client/v3/rooms/" (js/encodeURIComponent room-id)
+                 "/context/" (js/encodeURIComponent event-id) "?limit=0")]
+    (-> (js/fetch url #js {:headers #js {:Authorization (str "Bearer " token)}})
+        (p/then (fn [resp]
+                  (if (.-ok resp)
+                    (.json resp)
+                    (throw (js/Error. "Failed to fetch event context")))))
+        (p/then (fn [json]
+                  (let [event (.-event json)]
+                    {:body (get-in (js->clj event) ["content" "body"])
+                     :sender (or (.-sender event) "Someone")
+                     }))))))
+
+(defn- show-smart-notification [push-data token hs-url]
+  (let [sender        (or (.-sender_display_name push-data) (.-sender push-data) "Someone")
+        room-name     (.-room_name push-data)
+        room-id       (.-room_id push-data)
+        unread-count  (.-unread push-data)
+        encrypted?    (= (.-type push-data) "m.room.encrypted")
+        avatar-mxc    (or (some-> push-data .-content .-avatar_url)
+                          (some-> push-data .-sender_avatar_url))
+        body          (if encrypted?
+                        "🔒 Encrypted Message"
+                        (or (some-> push-data .-content .-body) "New Message"))
+        title         (if room-name (str sender " in " room-name) sender)]
+
+    (when (exists? js/navigator.setAppBadge)
+      (if (and unread-count (pos? unread-count))
+        (.setAppBadge js/navigator unread-count)
+        (when (= unread-count 0)
+          (.clearAppBadge js/navigator))))
+
+    (p/let [icon-url (fetch-avatar-url hs-url token avatar-mxc)]
+      (.showNotification js/self.registration title
+        #js {:body   body
+             :icon   (or icon-url default-icon)
+             :badge  default-badge
+             :tag    (or room-id "matrix-chat")
+             :data   #js {:room_id room-id}}))))
+
+(js/self.addEventListener "push"
+  (fn [event]
+    (let [data (try (.. event -data json) (catch :default _ nil))]
+      (when data
+        (let [user-id (.-user_id data)]
+          (.waitUntil event
+            (p/let [session (get-session-from-vault user-id)
+                    token   (some-> session .-token)
+                    hs-url  (some-> session .-hs_url)]
+              (show-smart-notification data token hs-url))))))))
+
+
+(js/self.addEventListener "notificationclick"
+  (fn [event]
+    (let [notification (.-notification event)
+          room-id      (.. notification -data -room_id)]
+      (.close notification)
+
+      (.waitUntil event
+        (p/let [clients (.matchAll js/self.clients #js {:type "window" :includeUncontrolled true})
+                tab (first clients)]
+          (if tab
+            (do
+              (.focus tab)
+              (.postMessage tab #js {:type "NAVIGATE_TO_ROOM"
+                                     :room_id room-id}))
+            (.openWindow js/self.clients "/")))))))
