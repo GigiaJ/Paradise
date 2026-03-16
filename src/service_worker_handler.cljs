007c9a73399f3f692957b2d7ab78348cfc62a1af
GigiaJ <gigiaboone@yahoo.com>
2026-03-16 18:53:33 -0500

    tangled sw

--- src/service_worker_handler.cljs
+++ src/service_worker_handler.cljs
@@ -29,24 +29,6 @@
                                  (re-frame/dispatch [:rooms/select room-id])))))))))
 
 
-
-#_(defn register-sw! [session]
-  (when (exists? js/navigator.serviceWorker)
-    (let [sw-container js/navigator.serviceWorker
-          try-sync!    (fn []
-                         (when (and (.-controller sw-container) session)
-                           (re-frame/dispatch [:auth/sync-sw-only session])))]
-
-      (-> (.register sw-container "/sw.js" #js {:type "module"})
-          (.then (fn [reg]
-                   (when (.-waiting reg)
-                     (.postMessage (.-waiting reg) #js {:type "SKIP_WAITING"})))))
-      (try-sync!)
-      (.addEventListener sw-container "controllerchange"
-                         (fn []
-                           (js/console.log "re-frame: SW Controller changed, syncing session...")
-                           (try-sync!))))))
-
 (re-frame/reg-event-fx
  :auth/sync-sw-only
  (fn [_ [_ session]]
